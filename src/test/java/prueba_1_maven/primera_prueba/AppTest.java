0ff4db42245f839b0a27119cd1e116c78bda58ad
Moises Sanchez <moy5921@gmail.com>
2022-09-06 16:26:11 -0500

    initial commit

---
+++ src/test/java/prueba_1_maven/primera_prueba/AppTest.java
@@ -0,0 +1,20 @@
+package prueba_1_maven.primera_prueba;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
